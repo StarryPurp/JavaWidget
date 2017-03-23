387d62f3b115e95cf8e7478e9488ec1e4e8e2c3e
StarryPurp <starbloom128@gmail.com>
2017-03-23 20:08:02 +0900

    #2 ChangeWidget in th Java

--- app/src/main/java/bstar128/example/hansung/javawidget/MainActivity.java
+++ app/src/main/java/bstar128/example/hansung/javawidget/MainActivity.java
@@ -1,7 +1,10 @@
 package bstar128.example.hansung.javawidget;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text1=(TextView) findViewById(R.id.text1);
+        TextView text2=(TextView) findViewById(R.id.text2);
+        TextView text3=(TextView) findViewById(R.id.text3);
+        TextView text4=(TextView) findViewById(R.id.text4);
+
+        text1.setText("First TextView");
+        text1.setTextSize(30);
+        text1.setBackgroundColor(Color.rgb(255,0,255));
+        text1.setTextColor(Color.WHITE);
+
+        text2.setText(R.string.text2_str);
+        text2.setTypeface(Typeface.SANS_SERIF,Typeface.BOLD);
+
+        text3.setTextSize(16);
+        text3.setText("Third TextView");
+        text3.setTextColor(Color.argb(200,0,0,255));
+        text3.setBackgroundColor(Color.MAGENTA);
+
+        text4.setText("Forth TextView");
+        text4.setTextSize(30);
+        text4.setBackgroundColor(Color.argb(170,200,0,0));
+
     }
 }
